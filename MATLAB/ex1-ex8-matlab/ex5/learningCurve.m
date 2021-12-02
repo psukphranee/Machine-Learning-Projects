2fffec6bfc63668dea9fd97e2507f726831f5db1
Panya Sukphranee <psukphranee@gmail.com>
2021-12-02 11:20:38 -0800

    finish section2

--- MATLAB/ex1-ex8-matlab/ex5/learningCurve.m
+++ MATLAB/ex1-ex8-matlab/ex5/learningCurve.m
@@ -53,9 +53,9 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 for i=1:m %loop through number of training samples
-   theta = trainLinearReg(X(1:i,:), y(1:i), 0);
+   theta = trainLinearReg(X(1:i,:), y(1:i), lambda);
    error_train(i) = linearRegCostFunction(X(1:i,:), y(1:i), theta, 0);
-   error_val(i) = linearRegCostFunction(Xval, yval, theta, lambda);
+   error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
 end
 
 
