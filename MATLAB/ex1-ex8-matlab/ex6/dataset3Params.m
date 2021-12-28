df30e32a26fb6b3449bddb3ec79725d10e6bc8e1
Panya Sukphranee <psukphranee@gmail.com>
2021-12-27 20:07:58 -0800

    svm grid search

--- MATLAB/ex1-ex8-matlab/ex6/dataset3Params.m
+++ MATLAB/ex1-ex8-matlab/ex6/dataset3Params.m
@@ -23,12 +23,39 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+%create vector of parameters to try out
+C_vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];%linspace(.1, 1, 10);
+sigma_vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]; %linspace(.1, 1, 10);
 
+%cartesian product of the above parameters
+[C_pts, sigma_pts] = meshgrid(C_vec, sigma_vec);
+C_sigma_prod = [C_pts(:), sigma_pts(:)];
 
+%loop through pair of values to see which gives the lowest values of error
+error = zeros(length(C_vec),1);
 
+min_error = intmax;
 
+for i=1:length(C_sigma_prod)
+    C_i = C_sigma_prod(i, 1);
+    sigma_i = C_sigma_prod(i, 2);
+    
+    model = svmTrain(X, y, C_i, @(x1, x2)gaussianKernel(x1, x2, sigma_i));
+    predictions = svmPredict(model, Xval);
 
+    error = mean(double(predictions ~= yval));
+    
+    if(error < min_error)
+        C = C_i;
+        sigma = sigma_i;
+        min_error = error;
+    end
+    
+end
 
+%min_index = find(error==min(error));
+%C = C_sigma_prod(min_index, 1);
+%sigma = C_sigma_prod(min_index, 2); 
 % =========================================================================
 
 end
