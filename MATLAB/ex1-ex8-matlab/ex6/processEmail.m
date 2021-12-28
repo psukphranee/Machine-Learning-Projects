6758ea92bece0c60481f889e234e79a8ffe300ce
Panya Sukphranee <psukphranee@gmail.com>
2021-12-27 23:22:30 -0800

    finish week 7

--- MATLAB/ex1-ex8-matlab/ex6/processEmail.m
+++ MATLAB/ex1-ex8-matlab/ex6/processEmail.m
@@ -96,8 +96,12 @@ while ~isempty(email_contents)
     % Note: You can use strcmp(str1, str2) to compare two strings (str1 and
     %       str2). It will return 1 only if the two strings are equivalent.
     %
-
-
+    index = find(strcmp(vocabList, str));
+    %disp(str);
+    if(isnumeric(index))
+        %disp(index)
+        word_indices = [word_indices; index];
+    end
 
 
 
